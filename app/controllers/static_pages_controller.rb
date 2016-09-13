75ff2a44d70f6cb188dfab07c6ea9152fc5ecee0
Lucas de Levy Oliveira <lucasdelevy@gmail.com>
2016-09-13 20:12:21 +0900

    Started moving website

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def welcome
+  end
+
+  def about_me
+  end
+
+  def projects
+  end
+
+  def game_of_life
+  end
+
+  def quaternions
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  root                     								'static_pages#welcome'
+  get		'/what-have-you-done'					=> 	'static_pages#projects'
+  get		'/game-of-life'							=> 	'static_pages#game_of_life'
+  get		'/has-matheus-portela-failed-already'	=>	'static_pages#has_matheus_portela_failed'
+  get     	'/who-are-you'							=>	'static_pages#about_me'
+  get     	'/blog'	 								=>	'posts#index'
+  get     	'/signup'  								=>	'users#new'
+  get     	'/login'   								=>	'sessions#new'
+  post    	'/login'   								=>	'sessions#create'
+  get	  	'/logout'  								=>	'sessions#destroy'
+  
+  resources :posts do
+    resources :comments
+  end
+end
