e61edb6111ac17be6fda18652b018383250a5978
Lucas de Levy Oliveira <lucasdelevy@gmail.com>
2016-09-30 19:22:11 +0900

    Added top button to posts

--- app/assets/javascripts/top_button.js
+++ app/assets/javascripts/top_button.js
@@ -1,32 +1,34 @@
-$(document).ready(function()
-{
+$(document).ready(
+  function()
+  {
     // hide #back-top first
     $("#back-top").hide();
 
     // fade in #back-top
     $(function ()
     {
-        $(window).scroll(function ()
+      $(window).scroll(function ()
+      {
+        if ($(this).scrollTop() > 100)
         {
-            if ($(this).scrollTop() > 100)
-            {
-                $('#back-top').fadeIn();
-            }
-            else
-            {
-                $('#back-top').fadeOut();
-            }
-        });
+          $('#back-top').fadeIn();
+        }
+        else
+        {
+          $('#back-top').fadeOut();
+        }
+      });
 
-        // scroll body to 0px on click
-        $('#back-top a').click(function ()
+      // scroll body to 0px on click
+      $('#back-top a').click(function ()
+      {
+        $('body,html').animate(
         {
-            $('body,html').animate(
-            {
-                scrollTop: 0
-            }, 800);
-            window.location.hash = '';
-            return false;
-        });
+            scrollTop: 0
+        }, 800);
+        window.location.hash = '';
+        return false;
+      });
     });
-});
+  }
+);
