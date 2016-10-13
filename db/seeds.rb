aab483a4857f771b16d96da19e6db9096178f25b
Lucas de Levy Oliveira <lucasdelevy@gmail.com>
2016-10-13 20:33:06 +0900

    Added seeds.rb again

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+JlptVocabulary.create(id: 1, kanji: '実験', hiragana: 'じっけん', english: 'experiment', level: 3)
+JlptVocabulary.create(id: 2, kanji: '妹', hiragana: 'いもうと', english: 'younger sister', level: 5)
